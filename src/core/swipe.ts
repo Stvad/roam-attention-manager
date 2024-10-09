058f715389099bdd9e555aecc3609359df1b5998
Vladyslav Sitalo <sitalov@amazon.com>
2024-10-09 12:33:47 -0700

    Add vertical threshold to prevent spurious swipe detection on vertical scroll

--- src/core/swipe.ts
+++ src/core/swipe.ts
@@ -4,21 +4,31 @@ export interface SwipeHandlers {
     onSwipeLeft?: SwipeEventHandler;
     onSwipeRight?: SwipeEventHandler;
     stopPropagation?: boolean;
+    threshold?: number;
+    maxVerticalThreshold?: number;
 }
 
 export const addSwipeListeners = (
     element: HTMLElement,
-    { onSwipeLeft, onSwipeRight, stopPropagation = false }: SwipeHandlers,
-    threshold: number = 50
+    {
+        onSwipeLeft,
+        onSwipeRight,
+        stopPropagation = false,
+        threshold = 100,
+        maxVerticalThreshold = 50,
+    }: SwipeHandlers
 ) => {
     let touchStartX: number = 0;
     let touchEndX: number = 0;
+    let touchStartY: number = 0;
+    let touchEndY: number = 0;
 
     const handleTouchStart = (event: TouchEvent): void => {
         if (stopPropagation) {
             event.stopPropagation();
         }
         touchStartX = event.changedTouches[0].screenX;
+        touchStartY = event.changedTouches[0].screenY;
     };
 
     const handleTouchEnd = (event: TouchEvent): void => {
@@ -26,16 +36,19 @@ export const addSwipeListeners = (
             event.stopPropagation();
         }
         touchEndX = event.changedTouches[0].screenX;
+        touchEndY = event.changedTouches[0].screenY;
         handleSwipeGesture(event);
     };
 
     const handleSwipeGesture = (event: Event): void => {
-        const distance = touchEndX - touchStartX;
+        const horizontalDistance = touchEndX - touchStartX;
+        const verticalDistance = Math.abs(touchEndY - touchStartY);
 
-        if (Math.abs(distance) > threshold) {
-            if (distance < 0 && onSwipeLeft) {
+        // Check that the swipe meets the horizontal threshold and does not exceed the vertical threshold
+        if (Math.abs(horizontalDistance) > threshold && verticalDistance <= maxVerticalThreshold) {
+            if (horizontalDistance < 0 && onSwipeLeft) {
                 onSwipeLeft(event);
-            } else if (distance > 0 && onSwipeRight) {
+            } else if (horizontalDistance > 0 && onSwipeRight) {
                 onSwipeRight(event);
             }
         }
